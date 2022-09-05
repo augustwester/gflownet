47ad5f47a7df244cab8ec6e263edc7b9a3839fef
August Wester <august.wester@gmail.com>
2022-09-05 14:57:28 +0200

    Remove detailed balance loss

--- gflownet/utils.py
+++ gflownet/utils.py
@@ -1,13 +1,5 @@
 import torch
 
-def detailed_balance_loss(fwd_probs, back_probs, term_probs, R):
-    lhs = (R[:, 0:-1] * fwd_probs * term_probs[:, 1:])
-    rhs = (R[:, 1:] * back_probs * term_probs[:, :-1])
-    lhs[lhs == 0] = 1
-    rhs[rhs == 0] = 1
-    loss = torch.log(lhs / rhs)**2
-    return loss.mean()
-
 def trajectory_balance_loss(total_flow, rewards, fwd_probs, back_probs):
     """
     Computes the mean trajectory balance loss for a collection of samples. For
